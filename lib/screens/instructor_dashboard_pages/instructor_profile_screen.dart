c26f6bb1738f469a5425c8d056a17bb0b1a3b88b
fawad kaleem <fwdkaleem@gmail.com>
2024-02-14 15:56:46 +0500

    Email Verify Done

--- lib/screens/instructor_dashboard_pages/instructor_profile_screen.dart
+++ lib/screens/instructor_dashboard_pages/instructor_profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -24,6 +25,161 @@ class _InstructorProfileScreenState extends State<InstructorProfileScreen> {
             child: Image.asset(
               "assets/logo.png",
               height: 250,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Dialog(
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(20.0)), //this right here
+                        child: Container(
+                          height: 300,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  "assets/logo.png",
+                                  height: 80,
+                                  width: 80,
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  'Do you want to verify the email',
+                                  style: TextStyle(
+                                    color: Color(0xFF1C1F34),
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Container(
+                                        width: 100,
+                                        height: 51,
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10, vertical: 16),
+                                        decoration: ShapeDecoration(
+                                          color: Colors.white,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(12),
+                                          ),
+                                        ),
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'No',
+                                              style: TextStyle(
+                                                color: Color(0xFF1C1F34),
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    InkWell(
+                                      onTap: () async {
+                                        User? user =
+                                            FirebaseAuth.instance.currentUser;
+                                        await user!
+                                            .sendEmailVerification()
+                                            .then((value) {
+                                          FirebaseFirestore.instance
+                                              .collection("instructors")
+                                              .doc(FirebaseAuth
+                                                  .instance.currentUser!.uid)
+                                              .update({"isVerified": true});
+                                        });
+
+                                        showSnakBar(
+                                            "Email Verification Link Send  Successfully",
+                                            context);
+                                      },
+                                      child: Container(
+                                        width: 100,
+                                        height: 51,
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10, vertical: 16),
+                                        decoration: ShapeDecoration(
+                                          color: bottomColor,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(12),
+                                          ),
+                                        ),
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'Yes',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    });
+              },
+              child: Text(
+                'Verify Email',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.acme(
+                  color: colorwhite,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: bottomColor,
+                  fixedSize: Size(301, 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
             ),
           ),
           const SizedBox(
@@ -215,7 +371,7 @@ class _InstructorProfileScreenState extends State<InstructorProfileScreen> {
                                         padding: const EdgeInsets.symmetric(
                                             horizontal: 10, vertical: 16),
                                         decoration: ShapeDecoration(
-                                          color: Color(0xFF40B59F),
+                                          color: bottomColor,
                                           shape: RoundedRectangleBorder(
                                             borderRadius:
                                                 BorderRadius.circular(12),
